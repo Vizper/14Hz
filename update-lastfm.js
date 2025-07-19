6868ec95a687253eb111aa27cf5551872c12a07e
14Hz <brownut.321@gmail.com>
2025-07-19 07:09:32 +0700

    Update update-lastfm.js

--- update-lastfm.js
+++ update-lastfm.js
@@ -1,5 +1,5 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
+import fetch from "node-fetch";
+import fs from "fs";
 
 const LASTFM_API_KEY = "c4e901f90da2a12ebb8b6d5cfa313b1f";
 const LASTFM_USERNAME = "Haze-path";
@@ -9,6 +9,11 @@ async function getRecentTracks() {
     const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LASTFM_USERNAME}&api_key=${LASTFM_API_KEY}&format=json&limit=${TRACK_LIMIT}`;
     const response = await fetch(url);
     const data = await response.json();
+
+    if (!data.recenttracks || !data.recenttracks.track) {
+        throw new Error("Failed to fetch recent tracks from Last.fm");
+    }
+
     return data.recenttracks.track.map((track, index) => {
         const artist = track.artist["#text"];
         const title = track.name;
@@ -17,19 +22,20 @@ async function getRecentTracks() {
 }
 
 async function updateReadme(tracks) {
-    const content = `### 🎧 Last Played Tracks\n\n${tracks.join("\n")}\n`;
-    const readme = fs.readFileSync("README.md", "utf8");
-    const updated = readme.replace(
-        /### 🎧 Last Played Tracks\n\n([\s\S]*?)(?=\n###|$)/,
-        content
-    );
-    fs.writeFileSync("README.md", updated);
+    const filePath = "README.md";
+    const readme = fs.readFileSync(filePath, "utf8");
+
+    const start = "<!-- LASTFM:START -->";
+    const end = "<!-- LASTFM:END -->";
+    const regex = new RegExp(`${start}[\\s\\S]*?${end}`, "g");
+
+    const newContent = `${start}\n${tracks.join("\n")}\n${end}`;
+    const updated = readme.replace(regex, newContent);
+
+    fs.writeFileSync(filePath, updated, "utf8");
     console.log("README.md updated!");
 }
 
 getRecentTracks()
-    .then((tracks) => {
-        console.log("Fetched tracks:", tracks);
-        updateReadme(tracks);
-    })
-    .catch((err) => console.error("Error fetching tracks:", err));
+    .then(updateReadme)
+    .catch((err) => console.error("Error:", err));
